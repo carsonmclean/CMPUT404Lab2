64f34fbb16a9f93ebc01384684ca1fc1b8f1c19a
Carson McLean <camclean@ualberta.ca>
2016-09-15 15:34:19 -0600

    Adds forking

--- proxy.py
+++ proxy.py
@@ -1,51 +1,57 @@
 #!/usr/bin/env python
 import socket
+import os
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.bind(("0.0.0.0", 8000))
 clientSocket.listen(5)
 clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-(incomingSocket, address) = clientSocket.accept()
-print "we got a connection from %s!" % (str(address))
-
-googleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-googleSocket.connect(("www.google.ca", 80))
-
-incomingSocket.setblocking(0)
-googleSocket.setblocking(0)
-
 while True:
-    # This half of the loop forwards from
-    # client to google
-    skip = False
-    try:
-        part = incomingSocket.recv(1024)
-    except socket.error, exception:
-        if exception.errno == 35:
-            skip = True
-        else:
-            raise
-    if not skip:
-        if (len(part) > 0):
-            print " > " + part
-            googleSocket.sendall(part)
-        else: # part will be "" when the connection is done
-            exit(0)
+    (incomingSocket, address) = clientSocket.accept()
+    print "we got a connection from %s!" % (str(address))
 
-    # This half of the loop forwards from
-    # google to the client
-    skip = False
-    try:
-        part = googleSocket.recv(1024)
-    except socket.error, exception:
-        if exception.errno == 35:
-            skip = True
-        else:
-            raise
-    if not skip:
-        if (len(part) > 0):
-            print " < " + part
-            incomingSocket.sendall(part)
-        else: # part will be "" when the connection is done
-            exit(0)
+    pid = os.fork()
+    if (pid == 0): # we must be the child (clone) process, so we will handle proxying for this client
+
+
+        googleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        googleSocket.connect(("www.google.ca", 80))
+
+        incomingSocket.setblocking(0)
+        googleSocket.setblocking(0)
+
+        while True:
+            # This half of the loop forwards from
+            # client to google
+            skip = False
+            try:
+                part = incomingSocket.recv(1024)
+            except socket.error, exception:
+                if exception.errno == 35:
+                    skip = True
+                else:
+                    raise
+            if not skip:
+                if (len(part) > 0):
+                    print " > " + part
+                    googleSocket.sendall(part)
+                else: # part will be "" when the connection is done
+                    exit(0)
+
+            # This half of the loop forwards from
+            # google to the client
+            skip = False
+            try:
+                part = googleSocket.recv(1024)
+            except socket.error, exception:
+                if exception.errno == 35:
+                    skip = True
+                else:
+                    raise
+            if not skip:
+                if (len(part) > 0):
+                    print " < " + part
+                    incomingSocket.sendall(part)
+                else: # part will be "" when the connection is done
+                    exit(0)
