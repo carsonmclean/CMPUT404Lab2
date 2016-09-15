a788472d6f2504936a9edc60a4e648b14e75d571
Carson McLean <camclean@ualberta.ca>
2016-09-15 15:05:47 -0600

    Progress

--- proxy.py
+++ proxy.py
@@ -3,30 +3,49 @@ import socket
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.bind(("0.0.0.0", 8000))
-clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 clientSocket.listen(5)
+clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 (incomingSocket, address) = clientSocket.accept()
 print "we got a connection from %s!" % (str(address))
 
 googleSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-googleSocket.connect(("www.google.com", 80))
+googleSocket.connect(("www.google.ca", 80))
+
+incomingSocket.setblocking(0)
+googleSocket.setblocking(0)
 
 while True:
     # This half of the loop forwards from
     # client to google
-    part = incomingSocket.recv(1024)
-    if (len(part) > 0):
-        print " > " + part
-        googleSocket.sendall(part)
-    else: # part will be "" when the connection is done
-        exit(0)
+    skip = False
+    try:
+        part = incomingSocket.recv(1024)
+    except socket.error, exception:
+        if exception.errno == 35:
+            skip = True
+        else:
+            raise
+    if not skip:
+        if (len(part) > 0):
+            print " > " + part
+            googleSocket.sendall(part)
+        else: # part will be "" when the connection is done
+            exit(0)
 
     # This half of the loop forwards from
     # google to the client
-    part = googleSocket.recv(1024)
-    if (len(part) > 0):
-        print " < " + part
-        incomingSocket.sendall(part)
-    else: # part will be "" when the connection is done
-        exit(0)
+    skip = False
+    try:
+        part = googleSocket.recv(1024)
+    except socket.error, exception:
+        if exception.errno == 35:
+            skip = True
+        else:
+            raise
+    if not skip:
+        if (len(part) > 0):
+            print " < " + part
+            incomingSocket.sendall(part)
+        else: # part will be "" when the connection is done
+            exit(0)
